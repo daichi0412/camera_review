0adfe101a093360d83c56b7a78a0e4f551dcec81
daichi0412 <1410030157w@gmail.com>
2022-07-16 18:05:10 +0900

    show-html-editing12

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,7 +12,7 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
     if @tweet.save
-      redirect_to root_path
+      redirect_to tweet_path
     else
       render :new
     end
